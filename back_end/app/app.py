b9b534e4554e4db1585aca697ea32121b3b4994e
yoonaiu <ginny42222@gmail.com>
2022-05-04 15:08:47 +0800

    delete redundant import

--- back_end/app/app.py
+++ back_end/app/app.py
@@ -1,19 +1,15 @@
-from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from flask_jwt_extended import JWTManager
 from flask_restful import Api, Resource
-from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, get_jwt, set_access_cookies
-from datetime import datetime
-from datetime import timedelta
-from datetime import timezone
 
 from app.resources.user import Auth, Auth_login
-import configSecret
+import config_secret
 
 
 app = Flask(__name__)
 
 # 1. connect to db
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/yoona/Documents/noteweb_backend/database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/yoona//4th_Sem/sql/HW2/UberEat-Booking-System/database.db'
 
 # 2. create table in db
 @app.before_first_request
@@ -24,7 +20,7 @@ def create_tables():
 
 # 3. jwt
 jwt = JWTManager(app)
-app.config['JWT_SECRET_KEY'] = configSecret.jwt_secret_key
+app.config['JWT_SECRET_KEY'] = config_secret.jwt_secret_key
 
 # 4. api & URL
 api = Api(app)
