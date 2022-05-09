c8263160dc2ee42c17aadaffa4f914270f4c5ce7
Angela Hsi <applehsi51102@gmail.com>
2022-05-09 23:21:30 +0800

    fix: sign in with fetch

--- frontend/js/indexJquery.js
+++ frontend/js/indexJquery.js
@@ -8,24 +8,29 @@ $(document).ready(function() {
        if (account === "" || password === "") {
            alert("登入失敗");
        } else {
-           $.ajax({
-               url: request_url + "/auth/login",
-               method: "POST",
-               dataType: "json",
-               data: {
-                   'account': account,
-                   'password': password,
-               },
-               success: function(data) {
-                   console.log(data);
-                   if (data === 200) { 
-                       window.location.replace("nav.html");
-                   } else {
-                       alert("登入失敗");
-                   }
-               }
-           });
-            // window.location.replace("nav.html");
+            let statusCode = null;
+            let headers = {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+            let body = {
+                'account': account,
+                'password': password,
+            }
+
+            fetch(request_url + "/auth/login", {
+                method: 'POST',
+                headers: headers,
+                body: JSON.stringify(body)
+            })
+            .then(function(response) {
+                statusCode = response['status'];
+                return response.json();
+            })
+            .then(function(myJson) {
+                if(statusCode === 200) window.location.replace("nav.html");
+                else alert("登入失敗");
+            })
         }
     });
 });
