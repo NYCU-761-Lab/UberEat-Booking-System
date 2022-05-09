9bc880845e355a5f569ceb59a76767a747c17b1c
yoonaiu <free.cs09@nycu.edu.tw>
2022-05-10 01:11:38 +0800

    add shop model

---
+++ backend/app/models/shop.py
@@ -0,0 +1,26 @@
+from app.db import db
+
+class ShopModel(db.Model):
+    __tablename__ = 'shop'
+
+    # now is no limit of length (not sure if is ok for sqlite) <fix>
+    shop_name = db.Column(db.String(256), unique = True, nullable = False, primary_key = True)
+    latitude = db.Column(db.Float(256), nullable = False)
+    longitude = db.Column(db.Float(256), nullable = False)
+    shop_type = db.Column(db.String(256), nullable = False)
+
+    # foreign key part, later, wail until we have other models
+
+    def __init__(self, shop_name, latitude, longitude, shop_type):
+        self.shop_name = shop_name
+        self.latitude = latitude
+        self.longitude = longitude
+        self.shop_type = shop_type
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def find_by_shop_name(cls, shop_name):
+        return cls.query.filter_by(shop_name=shop_name).first()
