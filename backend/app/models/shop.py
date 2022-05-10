ba5e300b1a04ec00282a60ad483f36d1784ec77f
yoonaiu <free.cs09@nycu.edu.tw>
2022-05-10 19:59:48 +0800

    fix comment

--- backend/app/models/shop.py
+++ backend/app/models/shop.py
@@ -2,15 +2,15 @@ from app.db import db
 
 class ShopModel(db.Model):
     __tablename__ = 'shop'
-
-    # now is no limit of length (not sure if is ok for sqlite) <fix>
+    
+    # string limit length: 256
     shop_name = db.Column(db.String(256), unique = True, nullable = False, primary_key = True)
     latitude = db.Column(db.Float(256), nullable = False)
     longitude = db.Column(db.Float(256), nullable = False)
     shop_type = db.Column(db.String(256), nullable = False)
 
     # foreign key part, later, wail until we have other models
-    # db.ForeignKey('table_name.primary_key')
+    # db.ForeignKey('table_name.primary_key'), table_name seems like no need to import
     user_account = db.Column(db.String(256), db.ForeignKey('user.account'), unique = True, nullable = False)
     
     def __init__(self, shop_name, latitude, longitude, shop_type):
