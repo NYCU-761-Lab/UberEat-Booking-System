d74cc6da01db0c8d06dc99ee335b3f8676d29d48
Angela Hsi <applehsi51102@gmail.com>
2022-05-15 02:22:25 +0800

    frontend: shop

--- backend/app/app.py
+++ backend/app/app.py
@@ -19,7 +19,7 @@ CORS(app)
 # basedir= os.path.abspath(os.path.dirname(__file__))
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.path.join(basedir,'data.sqlite')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/yoona/Documents/4th_Sem/sql/HW2_new/UberEat-Booking-System/backend/database.db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/angelahsi/UberEat-Booking-System/backend/database.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/angelahsi/Desktop/NYCU/大二下課程/資料庫/HW2/UberEat-Booking-System/backend/database.db'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
